462fbde3d016e75e92e5cfe13890b1658d3d30da
villasetaalluic <villaseta@hotmail.com>
2020-10-31 02:07:47 +0100

    CODE WORKS PROPERLY => starting testing Person struct handling

--- db_main.c
+++ db_main.c
@@ -17,25 +17,30 @@ int main() {
 
     create_db(&db);
 
-    import_CSV_country(&db);
-    export_CSV_country(&db);
-    load_country(&db);
-    print_country(&db);
+//    import_CSV_country(&db);
+//    export_CSV_country(&db);
+//    load_country(&db);
+//    print_country(&db);
+//
+//    import_CSV_industry(&db);
+//    export_CSV_industry(&db);
+//    load_industry(&db);
+//    print_industry(&db);
+//
+//    import_CSV_job(&db);
+//    export_CSV_job(&db);
+//    load_job(&db);
+//    print_job(&db);
+//
+//    import_CSV_group(&db);
+//    export_CSV_group(&db);
+//    load_group(&db);
+//    print_group(&db);
 
-    import_CSV_industry(&db);
-    export_CSV_industry(&db);
-    load_industry(&db);
-    print_industry(&db);
-
-    import_CSV_job(&db);
-    export_CSV_job(&db);
-    load_job(&db);
-    print_job(&db);
-
-    import_CSV_group(&db);
-    export_CSV_group(&db);
-    load_group(&db);
-    print_group(&db);
+    import_CSV_person(&db);
+    export_CSV_person(&db);
+    load_person(&db);
+    print_person(&db);
 
     return 0;
 }
