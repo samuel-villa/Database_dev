1de1be22ad5dd6f3116e6b616d1a908c5eec26ea
villasetaalluic <villaseta@hotmail.com>
2020-11-01 01:01:02 +0100

    implemented basic main_menu()

--- db_menus.c
+++ db_menus.c
@@ -13,6 +13,32 @@
 ****************************************************************************************/
 void main_menu(dbc *db) {
 
+    int menu_sel = 0;
+
     printf("\tDATABASE CLIENTS V0\n\n");
 
+    while (menu_sel != EXIT) {
+        printf("\t%d - Create Empty Database\n", CREATE_DB);
+        printf("\t%d - Open Database\n", OPEN_DB);
+        printf("\t%d - System Info\n", SYSTEM_INFO);
+        printf("\t%d - EXIT\n\n", EXIT);
+        printf("\t--> SELECT AN OPTION: "); scanf("%d", &menu_sel); fflush(stdin);
+
+        switch (menu_sel) {
+            case CREATE_DB:
+                create_db(db);
+                break;
+            case OPEN_DB:
+                puts("Opening db");
+                break;
+            case SYSTEM_INFO:
+                puts("Displaying System Info");
+                break;
+            case EXIT:
+                exit(0);
+            default:
+                puts("Wrong Selection");
+                break;
+        }
+    }
 }
